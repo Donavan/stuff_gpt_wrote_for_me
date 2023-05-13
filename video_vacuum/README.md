9df880d1d650dabcfc6ce4d3566220b384cf4dab
Donavan <snark@hey.com>
2023-05-13 16:47:11 -0400

    Complete replacement for vv using FFPMEG only

--- video_vacuum/README.md
+++ video_vacuum/README.md
@@ -23,13 +23,14 @@ The <directory> argument specifies the directory to search for video files.
 ## Options
 
 Here's a list of all available options:
-
-    -o, --output: The output folder for the compressed files. If not specified, the script uses the input directory as the output directory.
-    -l, --limit: The file size limit in GB. If a file is larger than this size, it will be compressed. Default is 8.0 GB.
-    -v, --vb: The the variable bitrate for Handbreak.
-    -p, --parallel: The number of parallel files to process. Default is 8.
-    -c, --compressor: The compressor tool to use. Can be either 'handbrake' or 'ffmpeg'. Default is 'handbrake'.
-    -b, --bitrate: The estimated bitrate for FFmpeg (e.g. 5000k). Default is '5000k'. This option is only used when FFmpeg is the chosen compressor.
+    --input: The location where the video files to process can be found.
+    --output: The location to put the new video files. This defaults to the input folder
+    --limit: The minimum size file to process from the input folder measured in gigabytes.
+    --threads: The number of threads FFMPEG will use
+    --size: How many gigabytes per hour of video at 1080p should the resulting file have. Defaults to 4.
+    --mask: A file mask for finding the filenames to process. should default to handling any mp4, mpg, avi or mkv file
+    --codec: The codec to use.  Defaults to libx264
+    --container: Force output to use this extension
 
 
 ## Examples
@@ -37,13 +38,13 @@ Here's a list of all available options:
 Here's an example command:
 
 ```bash
-python vacuum.py /home/user/videos -o /home/user/compressed_videos -l 10.0 -t 2.0 -p 4 -c ffmpeg -b 4000k
+python vv.py --input /home/user/videos --output /home/user/compressed_videos --limit 10 --size 2 --threads 4 
 ```
 
-This command will search for video files in /home/user/videos. If a file is larger than 10 GB, it will be compressed to around 2 GB and saved to /home/user/compressed_videos. The script will process up to 4 files in parallel. FFmpeg is used as the compressor with an estimated bitrate of 4000k.
+This command will search for video files in /home/user/videos. If a file is larger than 10 GB, it will be compressed to around 2 GB / hr  and saved to /home/user/compressed_videos.
 
 ## Requirements
 
     Python 3
-    HandBrakeCLI or FFmpeg
+    FFmpeg
     multiprocessing, subprocess, argparse, os, glob, functools, logging (These are standard Python libraries and should be included in most Python installations)
--- video_vacuum/vv.py
+++ video_vacuum/vv.py
@@ -1,127 +1,105 @@
-#!/usr/bin/env python3
 import os
-import sys
-import glob
+import math
+import shlex
+import fnmatch
 import logging
 import argparse
-import functools
 import subprocess
-import multiprocessing
-
-logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def compress_file_handbrake(file_path, output_folder, vb):
-    base_name = os.path.basename(file_path)
-    if output_folder != os.path.dirname(file_path):
-        output_file = os.path.join(output_folder, base_name + ".mkv")
-    else:
-        output_file = os.path.join(output_folder, base_name + "_compressed.mkv")
+class VideoVacuum:
+    def __init__(self, args):
+        self.input_folder = args.input
+        self.output_folder = args.output
+        self.limit = args.limit
+        self.target_size = args.size
+        self.file_mask = args.mask
+        self.threads = args.threads
+        self.codec = args.codec
+        self.container = args.container
 
-    command = [
-        "HandBrakeCLI",
-        "--input", file_path,
-        "--output", output_file,
-        "--encoder", "x265",
-        "--vb", str(vb),
-        "--quality", "26",
-        "--optimize",
-        "--encoder-preset", "medium"
-    ]
-    try:
-        subprocess.check_call(command)
-        # logging.info((' '.join(command)))
-        logging.info(f'Successfully compressed: {file_path}')
-    except subprocess.CalledProcessError as e:
-        logging.error(f'Compression failed for: {file_path}, error: {e}')
+    def process_folder(self):
+        for root, dirs, files in os.walk(self.input_folder):
+            for basename in files:
+                for extension in self.file_mask.split(','):
+                    if fnmatch.fnmatch(basename, extension):
+                        filename = os.path.join(root, basename)
+                        try:
+                            if os.path.getsize(filename) >= self.limit * (1024 ** 3):  # convert GB to bytes
+                                if not os.path.exists(os.path.join(self.input_folder, basename.rsplit('.', 1)[0] + '_c.' + basename.rsplit('.', 1)[1])):
+                                    self.process_file(filename)
+                        except OSError as e:
+                            logging.error(f"Error processing file {filename}: {e}")
 
+    @staticmethod
+    def convert_gb_per_hr_to_kbps(gb_per_hr):
+        kilobits_per_gb = 8_589_934.592
+        seconds_per_hour = 3600
+        return math.ceil(gb_per_hr * kilobits_per_gb / seconds_per_hour)
 
-def compress_file_ffmpeg(file_path, output_folder, estimated_bitrate):
-    base_name = os.path.basename(file_path)
-    if output_folder != os.path.dirname(file_path):
-        base_name = os.path.splitext(base_name)[0]
-    output_file = os.path.join(output_folder, base_name + "_compressed.mkv")
+    def probe_file(self, input_filename):
+        # Determine the resolution and duration of the video file
+        cmd = f'ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 {shlex.quote(input_filename)}'
+        output = subprocess.check_output(cmd, shell=True).decode('utf-8')
+        width, height = output.strip().split('x')
 
-    command_pass1 = [
-        "ffmpeg",
-        "-y",
-        "-i", file_path,
-        "-c:v", "libx265",
-        "-preset", "medium",
-        "-b:v", estimated_bitrate,
-        "-pass", "1",
-        "-f", "null", "/dev/null"  # Discard output of first pass
-    ]
-    command_pass2 = [
-        "ffmpeg",
-        "-i", file_path,
-        "-c:v", "libx265",
-        "-preset", "medium",
-        "-b:v", estimated_bitrate,
-        "-pass", "2",
-        output_file
-    ]
-    try:
-        subprocess.check_call(command_pass1)
-        subprocess.check_call(command_pass2)
-        logging.info(f'Successfully compressed: {file_path}')
-    except subprocess.CalledProcessError as e:
-        logging.error(f'Compression failed for: {file_path}, error: {e}')
+        width = float(width)
+        height = float(height)
+
+        return width * height
+
+    def process_file(self, input_filename):
+        print(f"Processing {input_filename}")
+
+        # Calculate the bitrate
+        resolution = self.probe_file(input_filename)
+
+        base_resolution = (1080 * 1920)
+        res_scale = resolution / base_resolution
+        target_bitrate = math.ceil(self.convert_gb_per_hr_to_kbps(self.target_size * res_scale))
+
+        print(f"Res: {resolution},  Rate: {target_bitrate}")
 
 
-def compress_file(file_path, output_folder, vb, tool, estimated_bitrate):
-    if tool.lower() == 'handbrake':
-        compress_file_handbrake(file_path, output_folder, vb)
-    elif tool.lower() == 'ffmpeg':
-        compress_file_ffmpeg(file_path, output_folder, estimated_bitrate)
+        # Prepare the output file path
+        basename = os.path.basename(input_filename)
+        filename, ext = os.path.splitext(basename)
+
+        if os.path.abspath(input_filename) == os.path.abspath(self.output_folder):
+            filename = f"{filename}_c"
+
+        if self.container is not None:
+            ext = f".{self.container}"
+
+        base_name = f"{filename}{ext}"
+
+        output_file = shlex.quote(os.path.join(self.output_folder, base_name))
+
+        # Use ffmpeg to transcode the input file into the output folder using the target bitrate
+        cmd = f'ffmpeg -i {shlex.quote(input_filename)} -c:v {self.codec} -threads {self.threads} -b:v {target_bitrate}k {output_file} -loglevel info -progress - 2>&1 >/dev/null'
+        print(cmd)
+        subprocess.call(cmd, shell=True)
+        print()
 
 
-def process_directory(directory, output_folder, file_size_limit, target_file_size, parallel_files, tool,
-                      estimated_bitrate, masks):
-    files = []
-    for mask in masks:
-        files.extend(glob.glob(os.path.join(directory, mask)))
-    large_files = []
-    for file in files:
-        size = os.path.getsize(file) / (1024 * 1024 * 1024)  # Size in GB
-        if size > file_size_limit:
-            large_files.append(file)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Video Vacuum')
+    parser.add_argument('--input', type=str, required=True, help='The location where the video files to process can be found.')
+    parser.add_argument('--output', type=str, default=None, help='The location to put the new video files. This should default to the input folder.')
+    parser.add_argument('--limit', type=int, required=True, help='The minimum size file to process from the input folder measured in gigabytes.')
+    parser.add_argument('--threads', type=int, default=1, help='The number of threads FFMPEG will use')
+    parser.add_argument('--size', type=int, default=4, help='How many gigabytes per hour of video at 1080p should the resulting file have. Defaults to 4.')
+    parser.add_argument('--mask', type=str, default='*.mp4,*.mpg,*.avi,*.mkv', help='A file mask for finding the filenames to process. should default to handling any mp4, mpg, avi or mkv file')
+    parser.add_argument('--codec', type=str, default="libx264", help='The codec to use.  Defaults to libx264')
+    parser.add_argument('--container', type=str, default=None, help='Force output to use this extension')
 
-    pool = multiprocessing.Pool(processes=parallel_files)
-    partial_compress_file = functools.partial(compress_file, output_folder=output_folder,
-                                              target_file_size=target_file_size, tool=tool,
-                                              estimated_bitrate=estimated_bitrate)
-    pool.map(partial_compress_file, large_files)
-    pool.close()
-    pool.join()
 
+    return parser.parse_args()
 
 def main():
-    parser = argparse.ArgumentParser(description="Recompress large video files using HandBrake CLI or FFmpeg")
-    parser.add_argument("directory", help="Directory to search for video files")
-    parser.add_argument("-o", "--output", help="Output folder for compressed files")
-    parser.add_argument("-l", "--limit", type=float, help="File size limit in GB", default=8.0)
-    parser.add_argument("-v", "--vb", type=float, help="Variable bitrate", default=8000)
-    parser.add_argument("-p", "--parallel", type=int, help="Number of parallel files to process", default=1)
-    parser.add_argument("-c", "--compressor", help="Compressor tool to use: 'handbrake' or 'ffmpeg'",
-                        default='handbrake')
-    parser.add_argument("-b", "--bitrate", help="Estimated bitrate for FFmpeg (e.g. 5000k)", default='5000k')
-    parser.add_argument("-m", "--mask", nargs='+', help="File masks to match, e.g. *.mkv *.mp4",
-                        default=["*.[Mm][Kk][Vv]", "*.[Mm][Pp]4", "*.[Aa][Vv][Ii]", "*.[Mm][Pp][Gg]",
-                                 "*.[Mm][Pp][Ee][Gg]"])
-    args = parser.parse_args()
-
-    if args.output is None:
-        output_folder = args.directory
-    else:
-        output_folder = args.output
-
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    process_directory(args.directory, output_folder, args.limit, args.vb, args.parallel, args.compressor, args.bitrate,
-                      args.mask)
-
+    args = parse_args()
+    video_vacuum = VideoVacuum(args)
+    video_vacuum.process_folder()
 
 if __name__ == "__main__":
     main()
